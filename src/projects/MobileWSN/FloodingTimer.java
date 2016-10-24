80d6c98210c992f965cc62db88d8d217d834a6fb
Josmario Albuquerque <josmarios.silva@gmail.com>
2016-10-24 01:10:00 -0300

    node position being updated

--- src/projects/MobileWSN/FloodingTimer.java
+++ src/projects/MobileWSN/FloodingTimer.java
@@ -4,39 +4,57 @@ import jsensor.runtime.Jsensor;
 import jsensor.nodes.Node;
 import jsensor.nodes.events.TimerEvent;
 import jsensor.utils.GenerateFilesOmnet;
-
+import jsensor.utils.Position;
 
 /**
  *
  * @author Danniel & Matheus
  */
-public class FloodingTimer extends TimerEvent{
+public class FloodingTimer extends TimerEvent {
 
-    @Override
-    public void fire() {
-    	
-    	Node destination = this.node.getRandomNode("FloodingNode");
-    	while(true){
-    		if(destination == null) {
-                destination = this.node.getRandomNode("FloodingNode");
-                continue;
-            }     
-            
-    		if(this.node == destination){
-    			destination = this.node.getRandomNode("FloodingNode");
-                continue;
-    		}
-    		break;
-    	}
-        
-        FloodingMessage message = new FloodingMessage(this.node, destination, 0, ""+this.node.getID(), this.node.getChunk());
-        
-        String messagetext = "Created by the sensor: "+Integer.toString(this.node.getID()) + " Path: ";
-        
-        message.setMsg(messagetext);
-    	Jsensor.log("time: "+ Jsensor.currentTime +"\t sensorID: "+this.node.getID()+ "\t sendTo: " +destination.getID());
-		
-    	GenerateFilesOmnet.addStartNode(this.node.getID(), destination.getID(), Jsensor.currentTime);
-	    this.node.multicast(message);
-    }    
+	private final int INCREMENT = 10;
+
+	@Override
+	public void fire() {
+
+		Node destination = this.node.getRandomNode("FloodingNode");
+		while (true) {
+			if (destination == null) {
+				destination = this.node.getRandomNode("FloodingNode");
+				continue;
+			}
+
+			if (this.node == destination) {
+				destination = this.node.getRandomNode("FloodingNode");
+				continue;
+			}
+			break;
+		}
+
+		FloodingMessage message = new FloodingMessage(this.node, destination, 0, "" + this.node.getID(),
+				this.node.getChunk());
+
+		String messagetext = "Created by the sensor: " + Integer.toString(this.node.getID()) + " Path: ";
+
+		message.setMsg(messagetext);
+		Jsensor.log("time: " + Jsensor.currentTime + "\t sensorID: " + this.node.getID() + "\t sendTo: "
+				+ destination.getID());
+
+		GenerateFilesOmnet.addStartNode(this.node.getID(), destination.getID(), Jsensor.currentTime);
+		this.node.multicast(message);
+
+		this.updateNodePostion(this.node);
+	}
+
+	/**
+	 * Updates a node position
+	 * 
+	 * @param node
+	 *            Node which its position is getting updated
+	 */
+	private void updateNodePostion(Node node) {
+		Position currentPos = node.getPosition();
+		Position newPos = new Position(currentPos.getPosX() + INCREMENT, currentPos.getPosY());
+		node.setPosition(newPos);
+	}
 }
